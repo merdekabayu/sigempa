8d9076545efe431b1e1374a87e80ff6d3f8bda09
merdekabayu <merdekabayu06@gmail.com>
2023-01-12 23:02:39 +0900

    b

--- sigempa.py
+++ sigempa.py
@@ -18,7 +18,7 @@ import subprocess,os
 import bcrypt
 
 with open('workdir.txt','w') as f:
-    py2output = subprocess.check_output(['whoami'])
+    py2output = subprocess.check_output('whoami')
     f.write(py2output)
     f.close()
 
