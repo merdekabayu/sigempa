ff2c03603ce24ecc33798f71f39e20ccc0adda93
merdekabayu <merdekabayu06@gmail.com>
2023-01-12 18:39:59 +0900

    p

--- fungsi/stat_sensor.py
+++ fungsi/stat_sensor.py
@@ -137,26 +137,13 @@ def slinktool():
         last_dt = datetime.strptime(dtime, '%Y/%m/%d %H:%M:%S.%f')
         delay = (today-last_dt).total_seconds()
 
-        last_data += [[name,last_dt,delay]]
+        last_data += [(name,last_dt,delay)]
         last_data1 = [[name,last_dt,delay]]
 
-        str1 = name1
-        var1 = locals()
-        var1[str1] += last_data1
-
-        str = name
-        var = locals()
-        var[str] += [delay]
+    print(last_data)
         
-        
-        if len(name) > 2:
-            minv = min(name)
-            indexmin = name.index(minv)
-            update += name1[indexmin]
-        print(minv)
 
- 
-    return update,today
+    return last_data,today
 
 
 
