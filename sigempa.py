7bd266e72783faba0597295521427b9b65034df8
merdekabayu <merdekabayu06@gmail.com>
2023-01-13 11:38:32 +0900

    v

--- sigempa.py
+++ sigempa.py
@@ -17,7 +17,7 @@ from fungsi.waveform import waveformplot, allwaveform, down_waveformbyevent
 import subprocess,os
 import bcrypt
 
-os.system('ls static/peta_diseminasi*.png')
+os.system('whoami')
 
 
 app = Flask(__name__)
