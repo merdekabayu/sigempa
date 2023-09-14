f1c981ffff63417bc282bf1b2df1628216360ae7
merdekabayu <merdekabayu06@gmail.com>
2023-09-14 18:22:07 +0900

    up

--- fungsi/skripdownloadfdsn.py
+++ fungsi/skripdownloadfdsn.py
@@ -5,8 +5,8 @@ from pytictoc import TicToc
 
 t = TicToc()
 t.tic()
-client = Client('http://36.91.152.130:8081', user='pgn', password='pgn1234')
-# client = Client('https://geof.bmkg.go.id/', user='bmkg', password='inatews2303#!3')
+# client = Client('http://36.91.152.130:8081', user='pgn', password='pgn1234')
+client = Client('https://geof.bmkg.go.id/', user='bmkg', password='inatews2303#!3')
 
 dtutc = datetime.utcnow()
 tnow = UTCDateTime(dtutc)
